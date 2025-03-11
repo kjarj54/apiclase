f988cd66879f0b9d376c237888ca430506ac05c9
Arauz <kjarj54@gmail.com>
2025-03-10 18:33:14 -0600

    Add CORS policy to the application

--- apiclase/Program.cs
+++ apiclase/Program.cs
@@ -1,15 +1,27 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add CORS policy
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin()
+             .AllowAnyHeader()
+             .AllowAnyMethod();
+    });
+});
 
+// Existing services configuration
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Enable CORS
+app.UseCors();
+
+// Rest of your middleware configuration
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -17,9 +29,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
